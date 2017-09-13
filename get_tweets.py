20195ef628f58c0b1a40d4edb7b954c38222d8ef
Kevin <kevpgoff@users.noreply.github.com>
2017-09-13 00:25:15 -0400

    more fixes

--- get_tweets.py
+++ get_tweets.py
@@ -20,8 +20,8 @@ access_secret = "XXXXX"
 parser = argparse.ArgumentParser(description="Scrape tweets")
 parser.add_argument('--username', type=str, required = True)
 parser.add_argument('--num', help='The number of tweets that will be scraped at each runtime', type=int, default=1, required = True)
-parser.add_argument('--fbmins', help='Number of minutes between posting loaded tweets to Facebook', type=int, default=60, required = True)
-parser.add_argument('--twittermins', help='Number of minutes between tweet scrapes', type=int, default=60, required = True)
+parser.add_argument('--fb-mins', help='Number of minutes between posting loaded tweets to Facebook', type=int, default=60, required = True)
+parser.add_argument('--twitter-mins', help='Number of minutes between tweet scrapes', type=int, default=60, required = True)
 
 
 def get_tweets(username):
@@ -40,7 +40,7 @@ def get_tweets(username):
 	auth.set_access_token(access_key, access_secret)
 	api = tweepy.API(auth)
 
-	number_of_tweets = 5
+	number_of_tweets = 15
 
 	tweets = api.user_timeline(screen_name = username,count = number_of_tweets)
 	
@@ -62,7 +62,6 @@ def get_tweets(username):
 		outfile.close()
 		
 def post_fb(username):
-	checker = False
 	ACCESS_TOKEN = ""
 	with open('keys.json') as json_file:
 		json_data = json.load(json_file)
@@ -75,8 +74,10 @@ def post_fb(username):
 	
 	with open('{0}_tweets.json'.format(username)) as json_file:
 		json_data = json.load(json_file)
+		json_file.close()
 	
 	for item in json_data["tweets"]:
+		checker = False
 		with open('{0}_posted.txt'.format(username), 'r+') as postedfiles:
 			for line in postedfiles:
 				if item["id"] in line:
@@ -85,16 +86,18 @@ def post_fb(username):
 			if checker == False:
 				postedfiles.write(item["id"] + "\n")
 				
+			postedfiles.close()
+			
 		if checker == False and "t.co" not in item["text"]:
 			graph.put_wall_post(item["text"])				
-			
+				
 					
 def main():
 	#args = parser.parse_args()
 	#IF RUNNING FROM CMD
     #if len(sys.argv) == 2:
-    #    schedule.every(twittermins).minutes.do(get_tweets(sys.argv[1]))
-	#	schedule.every(fbmins).minutes.do(post_fb(sys.argv[1])
+    #   schedule.every(twitter-mins).minutes.do(get_tweets(sys.argv[1]))
+	#	schedule.every(fb-mins).minutes.do(post_fb(sys.argv[1])
 	#	while True:
 	#	schedule.run_pending()
 	#	time.sleep(1)
