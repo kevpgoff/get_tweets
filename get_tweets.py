9c2e33b32a55aeef3f22de015034589a26263b43
Kevin <kevpgoff@users.noreply.github.com>
2017-09-18 19:56:43 -0400

    Support for images/gifs

--- get_tweets.py
+++ get_tweets.py
@@ -5,12 +5,16 @@ import sys
 import csv
 import time
 import mmap
+import os
+import subprocess
+import urllib.request
 
 import json
 import tweepy
 import schedule
 import argparse
 import facebook
+import magic
 
 consumer_key = "XXXXX"
 consumer_secret = "XXXXX"
@@ -40,9 +44,9 @@ def get_tweets(username):
 	auth.set_access_token(access_key, access_secret)
 	api = tweepy.API(auth)
 
-	number_of_tweets = 15
+	number_of_tweets = 3
 
-	tweets = api.user_timeline(screen_name = username,count = number_of_tweets)
+	tweets = api.user_timeline(screen_name = username,count = number_of_tweets, include_entities = True)
 	
 	
 	json_file.close()
@@ -55,6 +59,32 @@ def get_tweets(username):
 		'id': tweet.id_str,
 		'text': tweet.text.encode('utf-16', 'surrogatepass').decode('utf-16')
 		})
+
+	for status in tweets:
+		if 'media' in status.entities:
+			count = 0;
+			for media in status.entities['media']:
+				count += 1;
+				if media['type'] == 'photo':
+					image_uri = media['media_url'] + ':large'
+					filename = username + '-' + status.id_str + '-' + str(count)
+					filepath = './images/' + filename
+					# download image
+					urllib.request.urlretrieve(image_uri, filepath)
+					# identify mime type and attach extension
+					if os.path.exists(filepath):
+						mime = magic.from_file(filepath, mime=True)
+						if mime == "image/gif":
+							newfilepath = filepath + ".gif"
+						elif mime == "image/jpeg":
+							newfilepath = filepath + ".jpg"
+						elif mime == "image/png":
+							newfilepath = filepath + ".png"
+						else:
+							err = filepath + ": unrecgonized image type"
+							print_error(err)
+							continue
+						os.rename(filepath, newfilepath)
 		
 	print ("writing to {0}_tweets.json".format(username))
 	with open("{0}_tweets.json".format(username), 'w') as outfile:
@@ -69,8 +99,18 @@ def post_fb(username):
 	for item in json_data["keys"]:
 		if item["username"] == username:
 			ACCESS_TOKEN = item["access_token"]
+			pg_id = item["page_id"]
 			json_file.close()
+	#if facebook wall
 	graph = facebook.GraphAPI(ACCESS_TOKEN)
+	
+	#graph = facebook.GraphAPI(ACCESS_TOKEN)
+	#resp = graph.get_object('me/accounts')
+	#page_access_token = None
+	#for page in resp['data']:
+	#	if page['id'] == pg_id:
+	#		page_access_token = page['access_token']
+	#graph = facebook.GraphAPI(page_access_token)
 	
 	with open('{0}_tweets.json'.format(username)) as json_file:
 		json_data = json.load(json_file)
@@ -88,31 +128,33 @@ def post_fb(username):
 				
 			postedfiles.close()
 			
-		if checker == False and "t.co" not in item["text"]:
+		if checker == False and "t.co" not in item["text"] and "@" not in item["text"]:
 			graph.put_wall_post(item["text"])				
 				
 					
 def main():
 	#args = parser.parse_args()
 	#IF RUNNING FROM CMD
-    #if len(sys.argv) == 2:
-    #   schedule.every(twitter-mins).minutes.do(get_tweets(sys.argv[1]))
-	#	schedule.every(fb-mins).minutes.do(post_fb(sys.argv[1])
+	
+	if len(sys.argv) == 2:
+	#	schedule.every(5).minutes.do(get_tweets, sys.argv[1])
+	#	schedule.every(10).minutes.do(post_fb, sys.argv[1])
 	#	while True:
-	#	schedule.run_pending()
-	#	time.sleep(1)
+	#		schedule.run_pending()
+	#		time.sleep(1)	
+			
+		#above for scheduling, below for single run		
 		
-		#above for scheduling, below for single run
-		
-		#get_tweets(sys.argv[1])		
     #else:
     #    print ("Error: enter one username")
 	
-	if len(sys.argv) == 2:
+	#FOR MANUAL SINGLE RUN
 		get_tweets(sys.argv[1])
 		post_fb(sys.argv[1])
+	
 	else:
 		print ("Error: enter one username")	
+		
     #alternative method: loop through multiple users
 	# users = ['user1','user2']
 
